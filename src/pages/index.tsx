b6faa4409b32745a8b7a1c6f6b3bba1c68c76a71
Karl <enriquezkharl14@gmail.com>
2023-04-05 11:29:44 +0800

    added github auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
+import { NextPageContext } from 'next';
+import { useSession, signIn, signOut, getSession } from 'next-auth/react';
+
 export default function Home() {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <>
       <h1 className="text-red-700">hello wotld</h1>
+      <button onClick={() => signOut()}>Sign out</button>
     </>
   );
+}
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
