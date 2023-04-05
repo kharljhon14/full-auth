045156ab84f01cfc1742eaf3d19c1bf53e2826aa
Karl <enriquezkharl14@gmail.com>
2023-04-05 11:46:11 +0800

    added google auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
 import { NextPageContext } from 'next';
 import { useSession, signIn, signOut, getSession } from 'next-auth/react';
+import Image from 'next/image';
 
 export default function Home() {
   const { data: session } = useSession();
   console.log(session);
   return (
     <>
-      <h1 className="text-red-700">hello wotld</h1>
-      <button onClick={() => signOut()}>Sign out</button>
+      <h1 className="text-red-700">{session?.user?.name}</h1>
+      <img src={session?.user?.image as string} alt="avatar" className="w-1/8" />
+      {session ? (
+        <button onClick={() => signOut()}>Sign out</button>
+      ) : (
+        <button onClick={() => signIn()}>Sign in</button>
+      )}
     </>
   );
 }
