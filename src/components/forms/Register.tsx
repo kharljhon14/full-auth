c3645be9d4e247d73c5dd8171e8cc2f7884e7139
Karl <enriquezkharl14@gmail.com>
2023-04-07 19:47:53 +0800

    input component

--- src/components/forms/Register.tsx
+++ src/components/forms/Register.tsx
@@ -1,10 +1,11 @@
 import Input from '../inputs/Input';
+import { CiUser } from 'react-icons/ci';
 
 export default function Register() {
   return (
     <form action="" className="my-8 text-sm">
       <div className="gap-2 md:flex">
-        <Input name={''} label={''} type={'number'} icon={undefined} placeholder={''} />
+        <Input name={''} label={''} type="text" icon={<CiUser />} placeholder={'example'} />
       </div>
     </form>
   );
--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -10,8 +10,15 @@ interface Props {
 
 export default function Input({ name, label, type, icon, placeholder }: Props) {
   return (
-    <>
-      <input type="text" />
-    </>
+    <div className="relative mt-1 rounded-md shadow-sm">
+      <div className="pointer-event-none absolute left-0 inset-y-0 flex items-center pl-3">
+        <span className="text-gray-500 text-sm">{icon}</span>
+      </div>
+      <input
+        className="w-full py-2 pr-7 pl-8 block rounded-md border border-gray-300 outline-offset-2 outline-transparent focus:border-teal-400 focus:ring-indigo-700 focus:ring-2 text-sm"
+        type={type}
+        placeholder={placeholder}
+      />
+    </div>
   );
 }
