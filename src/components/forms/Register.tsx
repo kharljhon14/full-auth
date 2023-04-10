4ee8730012fa15a9e20b0c48eb3d46ecc811e8ef
Karl <enriquezkharl14@gmail.com>
2023-04-10 15:57:43 +0800

    form password

--- src/components/forms/Register.tsx
+++ src/components/forms/Register.tsx
@@ -1,6 +1,6 @@
 import Input from '../inputs/Input';
 import { CiUser } from 'react-icons/ci';
-import { FiMail } from 'react-icons/fi';
+import { FiMail, FiLock } from 'react-icons/fi';
 import { BsTelephone } from 'react-icons/bs';
 import { useForm } from 'react-hook-form';
 import { RegisterSchema, RegisterSchemaType } from '@/schemas/register';
@@ -26,7 +26,7 @@ export default function Register() {
           label={'First name'}
           type="text"
           icon={<CiUser />}
-          placeholder={'example'}
+          placeholder={'John'}
           register={register}
           error={errors?.first_name?.message}
           disabled={isSubmitting}
@@ -36,7 +36,7 @@ export default function Register() {
           label={'Last name'}
           type="text"
           icon={<CiUser />}
-          placeholder={'example'}
+          placeholder={'Doe'}
           register={register}
           error={errors?.last_name?.message}
           disabled={isSubmitting}
@@ -47,7 +47,7 @@ export default function Register() {
         label={'Email address'}
         type="email"
         icon={<FiMail />}
-        placeholder={'example'}
+        placeholder={'example@mail.com'}
         register={register}
         error={errors?.email?.message}
         disabled={isSubmitting}
@@ -57,11 +57,34 @@ export default function Register() {
         label={'Phone number'}
         type="number"
         icon={<BsTelephone />}
-        placeholder={'example'}
+        placeholder={'+(xxx) xxx-xx-xx'}
         register={register}
         error={errors?.phone?.message}
         disabled={isSubmitting}
       />
+      <div className="gap-2 md:flex">
+        <Input
+          name={'password'}
+          label={'Password'}
+          type="password"
+          icon={<FiLock />}
+          placeholder={'********'}
+          register={register}
+          error={errors?.password?.message}
+          disabled={isSubmitting}
+        />
+        <Input
+          name={'confirm_password'}
+          label={'Confirm password'}
+          type="password"
+          icon={<FiLock />}
+          placeholder={'********'}
+          register={register}
+          error={errors?.confirm_password?.message}
+          disabled={isSubmitting}
+        />
+      </div>
+
       <button>Submit</button>
     </form>
   );
--- src/schemas/register.ts
+++ src/schemas/register.ts
@@ -1,19 +1,29 @@
 import { z } from 'zod';
 import validator from 'validator';
 
-export const RegisterSchema = z.object({
-  first_name: z
-    .string()
-    .min(2, 'First name must be atleast 2 characters')
-    .max(32, 'Must be less tha 32 characters')
-    .regex(new RegExp('[a-zA-z]+$'), 'No Special characters allowed'),
-  last_name: z
-    .string()
-    .min(2, 'Last name must be atleast 2 characters')
-    .max(32, 'Must be less tha 32 characters')
-    .regex(new RegExp('[a-zA-z]+$'), 'No Special characters allowed'),
-  email: z.string().email('Please enter valid email address'),
-  phone: z.string().refine(validator.isMobilePhone, { message: 'Please enter a valid phone number' }),
-});
+export const RegisterSchema = z
+  .object({
+    first_name: z
+      .string()
+      .min(2, 'First name must be atleast 2 characters')
+      .max(32, 'Must be less tha 32 characters')
+      .regex(new RegExp('[a-zA-z]+$'), 'No Special characters allowed'),
+    last_name: z
+      .string()
+      .min(2, 'Last name must be atleast 2 characters')
+      .max(32, 'Must be less tha 32 characters')
+      .regex(new RegExp('[a-zA-z]+$'), 'No Special characters allowed'),
+    email: z.string().email('Please enter valid email address'),
+    phone: z.string().refine(validator.isMobilePhone, { message: 'Please enter a valid phone number' }),
+    password: z
+      .string()
+      .min(10, 'Password must be 10 characters')
+      .max(100, 'Password must be less than 100 characters'),
+    confirm_password: z.string(),
+  })
+  .refine((data) => data.password === data.confirm_password, {
+    message: "Password doesn't match",
+    path: ['confirm_password'],
+  });
 
 export type RegisterSchemaType = z.infer<typeof RegisterSchema>;
