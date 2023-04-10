4ef873350a501397921d64c3bf1375833b7d0d62
Karl <enriquezkharl14@gmail.com>
2023-04-10 15:24:09 +0800

    zod validation

--- src/components/forms/Register.tsx
+++ src/components/forms/Register.tsx
@@ -1,5 +1,7 @@
 import Input from '../inputs/Input';
 import { CiUser } from 'react-icons/ci';
+import { FiMail } from 'react-icons/fi';
+import { BsTelephone } from 'react-icons/bs';
 import { useForm } from 'react-hook-form';
 import { RegisterSchema, RegisterSchemaType } from '@/schemas/register';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -29,7 +31,37 @@ export default function Register() {
           error={errors?.first_name?.message}
           disabled={isSubmitting}
         />
+        <Input
+          name={'last_name'}
+          label={'Last name'}
+          type="text"
+          icon={<CiUser />}
+          placeholder={'example'}
+          register={register}
+          error={errors?.last_name?.message}
+          disabled={isSubmitting}
+        />
       </div>
+      <Input
+        name={'email'}
+        label={'Email address'}
+        type="email"
+        icon={<FiMail />}
+        placeholder={'example'}
+        register={register}
+        error={errors?.email?.message}
+        disabled={isSubmitting}
+      />
+      <Input
+        name={'phone'}
+        label={'Phone number'}
+        type="number"
+        icon={<BsTelephone />}
+        placeholder={'example'}
+        register={register}
+        error={errors?.phone?.message}
+        disabled={isSubmitting}
+      />
       <button>Submit</button>
     </form>
   );
--- src/schemas/register.ts
+++ src/schemas/register.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+import validator from 'validator';
 
 export const RegisterSchema = z.object({
   first_name: z
@@ -6,6 +7,13 @@ export const RegisterSchema = z.object({
     .min(2, 'First name must be atleast 2 characters')
     .max(32, 'Must be less tha 32 characters')
     .regex(new RegExp('[a-zA-z]+$'), 'No Special characters allowed'),
+  last_name: z
+    .string()
+    .min(2, 'Last name must be atleast 2 characters')
+    .max(32, 'Must be less tha 32 characters')
+    .regex(new RegExp('[a-zA-z]+$'), 'No Special characters allowed'),
+  email: z.string().email('Please enter valid email address'),
+  phone: z.string().refine(validator.isMobilePhone, { message: 'Please enter a valid phone number' }),
 });
 
 export type RegisterSchemaType = z.infer<typeof RegisterSchema>;
